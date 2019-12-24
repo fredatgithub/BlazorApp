9e63ec2b12e2464d39ebe6a1aaa067c8ecd40093
curious.drive <curious.drive@gmail.com>
2019-12-24 11:04:36 -0500

    Layouts Login Pages -

--- BlazorApp/BlazorApp/Data/User.cs
+++ BlazorApp/BlazorApp/Data/User.cs
@@ -11,5 +11,6 @@ namespace BlazorApp.Data
     {   
         public string EmailAddress { get; set; }
         public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
